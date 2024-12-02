c4dd3423efee1a9016b70b6ec83c4e9cc871e937
ClovisSecurity <126100008+ClovisSecurity@users.noreply.github.com>
2024-12-02 07:30:48 -0700

    Update analyze_codex_alex.py

--- analyze_codex_alex.py
+++ analyze_codex_alex.py
@@ -1,5 +1,16 @@
 import time
 import io
+##########################################################################################
+################### The Following Alphabet Can Be Used to Search: ########################
+##########################################################################################
+###  ALPHABET: 
+### λ, B, Γ, Ε, C, Υ, Χ, ι, Ο, Θ, Η, Ρ, λ, Δ, Τ, Ξ, Ζ, K, ψ, Ν, Φ, ω
+###
+### The only characters available to use for flex searches are below: 
+### FLEX ALPHABET: 
+### B, Γ, Υ, ι(Also MARK INDICATOR), Ο(C, E, Θ), Η(N), Ρ, λ(α, Δ), Τ, Ξ, Ζ, K, ψ, Φ, ω, Χ
+###########################################################################################
+###########################################################################################
 
 # Stores values to search for
 ky_search_str1 = "ΚΥCΗΤ"
@@ -7,7 +18,9 @@ ky_search_str2 = "ΚΥCλΤ"
 sat_search_str1 = "CλΤ"
 sat_search_str2 = "CΗΤ"
 
+# Values were CAT or CHT were found
 sat_arr = []
+# Values where KYCAT or KYCHT were found
 kysat_arr = []
 count = 0
 
@@ -27,13 +40,13 @@ bible_lines_arr = []
 # This should not be the full name of file
 output_filename = 'search_bible'
 
+# Open file and store lines in array
 with open(bible_filename,'r',encoding='utf8') as file:
     for item in file:
         if len(item) != 0:
             bible_lines_arr = bible_lines_arr + [item]
 
-# Loop through line by line and find values we're looking for and then add them
-# to an array
+# Loop through line by line and find values we're looking for and then add them to array
 for line in bible_lines_arr:
     count = count + 1
     if ky_search_str1 in line:
@@ -51,6 +64,7 @@ for line in bible_lines_arr:
 
 print("length of sat_arr is", len(sat_arr))
 print("length of kysat_arr is", len(kysat_arr))
+
 # This should contain location of index where value was found
 print("sat_loc_arr value", str(sat_loc_arr))
 print("kysat_loc_arr value", str(kysat_loc_arr))
@@ -64,8 +78,9 @@ for line in kysat_arr:
 # Take a slice of a timestamp for filename
 slice_time = str(time.time())[:10]
 
+# Create filename that will be used for exporting data
 output_filename = output_filename + slice_time + ".txt"
 
-
+# I haven't done this yet
 with io.open(output_filename,'w',encoding='utf8') as f:
     f.write(sep_text)
