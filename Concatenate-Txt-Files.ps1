ae5e46cde3da4918f7a93ed0de057ce8a5c16dfc
Zoo On <126100008+ClovisSecurity@users.noreply.github.com>
2024-12-13 00:39:51 -0700

    Easily Combine UTF8 Files in Powershell

--- Concatenate-Txt-Files.ps1
+++ Concatenate-Txt-Files.ps1
@@ -1,2 +1,1 @@
-# Easy as it gets...
- Get-Content sec1-codex-ocr.txt, sec2-codex-ocr.txt, sec3-codex-ocr.txt, sec4-codex-ocr.txt, sec5-codex-ocr.txt, sec6-codex-ocr.txt -Encoding UTF8 | out-file full-codex12-12.txt
+Get-Content sec1-codex-ocr.txt, sec2-codex-ocr.txt, sec3-codex-ocr.txt, sec4-codex-ocr.txt, sec5-codex-ocr.txt, sec6-codex-ocr.txt -Encoding UTF8 | out-file full-codex12-12.txt -Encoding UTF8
