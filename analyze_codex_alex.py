7164b0ddc52794cdac33d8cb70c0a3f4db4f9fd5
Zoo On <126100008+ClovisSecurity@users.noreply.github.com>
2024-12-21 14:27:24 -0700

    Easier to find search-word now

--- analyze_codex_alex.py
+++ analyze_codex_alex.py
@@ -1,20 +1,23 @@
 import time
 import io
 import os
-##################################################################################################
-##################### The Following Alphabet Can Be Used to Search: ##############################
-##################################################################################################
-#####  ALPHABET:                                                                                  #####
-#####  λ, B, Γ, Ε, C, Υ, Χ, ι, Ο, Θ, Η, Μ, Ρ, λ, Δ, Τ, Ξ, , K, ψ, Ν, Φ, ω, ΚΥ, Π                  #####
-#####                                                                                             #####
-##### The only characters available to use for flex searches are below:                           #####
-##### FLEX ALPHABET:                                                                              #####
-##### B, Γ, Υ, ι(Also MARK INDICATOR), Ο(C, E, Θ), Η(N), Ρ,Μ, λ(α, Δ), Τ, Ξ, Ζ, K, ψ, Φ, ω, Χ, Π  #####
-##################################################################################################
-##################################################################################################
+####################################################################################################################
+##################### The Following Alphabet Can Be Used to Search Greek Docs: #####################################
+####################################################################################################################
+#####  ALPHABET:                                                                                               #####
+#####  λ, B, Γ, Ε, C, Υ, Χ, ι, Ο, Θ, Η, Μ, Ρ, λ, Δ, Τ, Ξ, , K, ψ, Ν, Φ, ω, ΚΥ, Π, N                            #####
+#####                                                                                                          #####
+##### The only characters available to use for flex searches are below:                                        #####
+##### FLEX ALPHABET:                                                                                           #####
+##### B, Γ, Υ, ι(Also MARK INDICATOR), Ο(C, E, Θ), Η(N), Ρ, Μ, λ(α, Δ), Τ, Ξ, Ζ, K, ψ, Φ, ω, Χ, Π              #####
+#####                                                                                                          #####
+##### English Search Alphabet:                                                                                 #####
+##### A, B, Q, E, D, F, I, J, K, λ, L (for Theta),R (rho), P (pi), M, N, T, Z, PS, O (omega), @ (omicron), X,  #####
+####################################################################################################################
+####################################################################################################################
 
-# Stores value to search for
-word_search_str1 = 'λΕΝλ'
+# Stores value to search for λλRλREUT
+word_search_str1 = 'PAIAE'
 
 # Values where CAT or CHT were found
 word_arr = []
@@ -28,7 +31,11 @@ word_loc_arr = []
 # You can change the name of this file to use the code on different files.
 # NOTE: All files must be in the same directory
 # This must be the full name of file
-bible_filename = 'best-codec-ocr-stripped.txt'
+bible_filename = 'codex-12-19-5AM.txt'
+
+# All results stored in same directory
+output_dest_dir = './eng-codex-search-results/'
+
 
 # The Bible you import will be stored here
 bible_lines_arr = []
@@ -70,6 +77,7 @@ print("length of word_arr is", len(word_arr))
 goal_arr = []
 counter_tup = 0
 
+
 # trying to access text surrounding the pattern matches that
 # we detected during the word search
 for larger_text in index_tup_arr:
@@ -81,57 +89,60 @@ for larger_text in index_tup_arr:
             #            v                   v
             # print(larger_text + index_tup_arr[counter_tup])
 
-            # get lines surrounding the word the users searched for
+            # get lines surrounding the word the users searched for   
             try:
-                section0 = str(index_tup_arr[counter_tup - 5][1])
+                section0 = str(index_tup_arr[counter_tup - 5][1])  
             except IndexError:
                 pass
             try:
-                section1 = str(index_tup_arr[counter_tup - 4][1])
+                section1 = str(index_tup_arr[counter_tup - 4][1])  
             except IndexError:
                 pass
             try:
-                section2 = str(index_tup_arr[counter_tup - 3][1])
+                section2 = str(index_tup_arr[counter_tup - 3][1])  
             except IndexError:
                 pass
             try:
-                section3 = str(index_tup_arr[counter_tup - 2][1])
+                section3 = str(index_tup_arr[counter_tup - 2][1])  
             except IndexError:
                 pass
             try:
-                section4 = str(index_tup_arr[counter_tup - 1][1])
+                section4 = str(index_tup_arr[counter_tup - 1][1])  
             except IndexError:
                 pass
             try:
-                section5 = str(index_tup_arr[counter_tup][1])
+                section5 = str(index_tup_arr[counter_tup][1])  
             except IndexError:
                 pass
             try:
-                section6 = str(index_tup_arr[counter_tup + 1][1])
+                section6 = str(index_tup_arr[counter_tup + 1][1])  
             except IndexError:
                 pass
             try:
-                section7 = str(index_tup_arr[counter_tup + 2][1])
+                section7 = str(index_tup_arr[counter_tup + 2][1])  
             except IndexError:
                 pass
             try:
-                section8 = str(index_tup_arr[counter_tup + 3][1])
+                section8 = str(index_tup_arr[counter_tup + 3][1])  
             except IndexError:
                 pass
             try:
-                section9 = str(index_tup_arr[counter_tup + 4][1])
+                section9 = str(index_tup_arr[counter_tup + 4][1])  
             except IndexError:
                 pass
             try:
-                section10 = str(index_tup_arr[counter_tup + 5][1])
+                section10 = str(index_tup_arr[counter_tup + 5][1])  
             except IndexError:
                 pass
             try:
-                section11 = str(index_tup_arr[counter_tup + 6][1])
+                section11 = str(index_tup_arr[counter_tup + 6][1])  
             except IndexError:
-                pass
+                pass           
+            
+            
+            
 
-                # Concatenates fields near the field where the value searched for was found.
+            # Concatenates fields near the field where the value searched for was found.
             concat_str_entry = section0 + section1 + section2 + section3 + section4 + section5 + section6 + section7 + section8 + section9 + section10 + section11
 
             # print(concat_str_entry)
@@ -140,10 +151,10 @@ for larger_text in index_tup_arr:
 
     counter_tup = counter_tup + 1
 
-print(goal_arr)
+# print(goal_arr)
 
 # Replace all occurences of search string with string surrounded by spaces.
-replace_str = ' ' + word_search_str1 + ' '
+replace_str = ' **' + word_search_str1 + '** '
 
 new_arr = []
 
@@ -153,16 +164,15 @@ for item in goal_arr:
         new_arr = new_arr + [replaced_str]
     else:
         new_arr = new_arr + [item]
+        
 # Take a slice of a timestamp for filename
 slice_time = str(time.time())[:10]
-
-# All results stored in same directory
-output_dest_dir = './codex-search-results/'
 
 if os.path.isdir(output_dest_dir):
     print(f"Directory '{output_dest_dir}' exists.\n\nSaving Search Results...\n\nResults Saved")
 else:
     os.mkdir(output_dest_dir)
+ 
 
 # Create filename that will be used for exporting data
 output_filename = output_dest_dir + word_search_str1 + output_filename + slice_time + ".txt"
@@ -177,5 +187,6 @@ with open(output_filename, "w", encoding="utf-8") as file:
     file.write(top_line)
     for line in new_arr:
         # Write each line to the file, adding a newline character
-        file.write(
-            line + "\n" + "\n*****************************************************\n****************************************************\n\n")
+        file.write(line + "\n" + "\n*****************************************************\n**********************************************************\n\n")
+
+print(word_search_str1)
